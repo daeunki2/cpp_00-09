6fc6528103502e1aa578670069997a8fe875fc1a
daeunki2 <daeunki2@daeunki2-air.home>
2025-09-23 14:09:36 +0200

    code

--- cpp_08/ex00/inc/Easyfind.hpp
+++ cpp_08/ex00/inc/Easyfind.hpp
@@ -6,7 +6,28 @@
 /*   By: daeunki2 <daeunki2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 10:43:09 by daeunki2          #+#    #+#             */
-/*   Updated: 2025/09/22 10:43:10 by daeunki2         ###   ########.fr       */
+/*   Updated: 2025/09/22 10:57:30 by daeunki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stdexcept>
+
+template <typename T>
+typename T::iterator easyfind(T& container, int value)
+{
+    typename T::iterator it = std::find(container.begin(), container.end(), value);
+
+    if (it == container.end())
+	{
+        throw std::runtime_error("No occurrence found in the container.");
+    }
+    
+    return it;
+}
+#endif
--- cpp_08/ex00/main.cpp
+++ cpp_08/ex00/main.cpp
@@ -6,7 +6,33 @@
 /*   By: daeunki2 <daeunki2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 10:43:00 by daeunki2          #+#    #+#             */
-/*   Updated: 2025/09/22 10:43:01 by daeunki2         ###   ########.fr       */
+/*   Updated: 2025/09/22 10:59:13 by daeunki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Easyfind.hpp"
+
+int main()
+{
+    std::vector<int> myVector;
+    myVector.push_back(10);
+    myVector.push_back(20);
+    myVector.push_back(30);
+    myVector.push_back(40);
+
+    try
+	{
+        std::vector<int>::iterator result = easyfind(myVector, 30);
+        std::cout << "Element found: " << *result << std::endl;
+
+        std::vector<int>::iterator result2 = easyfind(myVector, 99);
+        std::cout << "Element found: " << *result2 << std::endl;
+
+    } 
+	catch (const std::runtime_error& e)
+	{
+        std::cout << "Error: " << e.what() << std::endl;
+    }
+    
+    return 0;
+}
