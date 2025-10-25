bb74838a393cc0f02549eb60ac0409882bbc9c7b
daeunki2 <daeunki2@daeunki2-air.home>
2025-10-25 15:15:34 +0200

    a

--- cpp_09/ex00/src/BitcoinExchange_Parser.cpp
+++ cpp_09/ex00/src/BitcoinExchange_Parser.cpp
@@ -6,22 +6,33 @@
 /*   By: daeunki2 <daeunki2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 15:18:09 by daeunki2          #+#    #+#             */
-/*   Updated: 2025/10/22 15:44:11 by daeunki2         ###   ########.fr       */
+/*   Updated: 2025/10/24 23:41:17 by daeunki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
-void parse_db(const std::string& filename, std::map<std::string, float>& container)
+void BitcoinExchange::parse_db(const std::string& filename, std::map<std::string, float>& container)
 {
-	std::string lne;
-}	line = getline()
+    std::ifstream db_file(filename);
+    
+    if (!db_file.is_open())
+    {
+        throw BitcoinExchange::Bitcoin_Error("Error: can't open database file.\n");
+    }
+    
+    std::string line;
+    
+    std::getline(db_file, line);
+	if (line != "date,exchange_rate")
+		throw BitcoinExchange::Bitcoin_Error("Error: first line. should be [date,exchange_rate].\n");
 
-
-
-void parse_search(const std::string& filename, std::multimap<std::string, float>& container)
-{
-
-
-	
+    while (std::getline(db_file, line))
+    {
+        if (line.empty() || line[0] == '#')
+            throw BitcoinExchange::Bitcoin_Error("Error: db does not allow empty line in middle.\n");
+    }
 }
+
+
+
